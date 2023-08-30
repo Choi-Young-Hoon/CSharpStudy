e5265ef5e9ddd8f36706afa3ec8dd84356e6fc77
Choi-Young-Hoon <hun5077@gmail.com>
2023-08-30 18:16:18 +0900

    LinkedList

---
+++ CSharpStudy/DataStructure/DataStructureStudy/CustomLinkedList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.DataStructure
+{
+    internal class CustomLinkedList<T>
+    {
+        class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+            public Node Prev { get; set; }
+
+            public Node(T data)
+            {
+                this.Data = data;
+                this.Next = null;
+                this.Prev = null;
+            }
+             ~Node()
+            {
+                Console.WriteLine("Test Print Destroy");
+            }
+
+            public static Node CreateNode(T data)
+            {
+                return new Node(data);
+            }
+        }
+
+        private Node _rootNode;
+        private Node _backNode;
+        public int Count { get; set; }
+        
+
+        public CustomLinkedList()
+        {
+            this.Count = 0;
+            this._rootNode = null;
+        }
+
+        public void PushBack(T data)
+        {
+            Node node = Node.CreateNode(data);
+            if (this.Count == 0)
+            {
+                this._rootNode = node;
+                this._backNode = node;
+            }
+            else
+            {
+                this._backNode.Next = node;
+                node.Prev = this._backNode;
+                this._backNode = node;
+            }
+            this.Count++;
+        }
+
+        public void PushFront(T data)
+        {
+            Node node = Node.CreateNode(data);
+            if (this.Count == 0)
+            {
+                this._rootNode = node;
+            }
+            else
+            {
+                this._rootNode.Prev = node;
+                node.Next = this._rootNode;
+                this._rootNode = node;
+            }
+            this.Count++;
+        }
+
+        public void Clear()
+        {
+            this._rootNode = null;
+            this._backNode = null;
+            this.Count = 0;
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine($"COUNT: {this.Count}");
+            for (Node node = this._rootNode; node != null; node = node.Next)
+            {
+                Console.WriteLine(node.Data);
+            }
+        }
+    }
+}
---
+++ CSharpStudy/DataStructure/ProgramMain.cs
@@ -0,0 +1,30 @@
+﻿using DataStructure.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class ProgramMain
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList<int> customLinkedList = new CustomLinkedList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                customLinkedList.PushBack(i);
+            }
+            customLinkedList.PushFront(100);
+            customLinkedList.PushFront(200);
+
+            customLinkedList.PrintAll();
+
+            Console.WriteLine("-=======");
+            customLinkedList.Clear();
+            Console.ReadLine();
+            Console.WriteLine("========");
+        }
+    }
+}
